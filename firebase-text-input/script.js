c15a1db8efc7ace08def1f7a4b278e9f3420f1b8
leeechloe <celee@usc.edu>
2026-04-14 18:36:01 -0700

    firebase

---
+++ firebase-text-input/script.js
@@ -0,0 +1,67 @@
+// Your web app's Firebase configuration
+//const firebaseConfig = {
+//  apiKey: "AIzaSyAFajdu_Y13KgwcZA3LPJmj2j_nw7SoB0s",
+//  authDomain: "collective-input.firebaseapp.com",
+//  projectId: "collective-input",
+//  storageBucket: "collective-input.appspot.com",
+//  messagingSenderId: "338519851864",
+//  appId: "1:338519851864:web:5fb3b64d1cad63b20b1b2d",
+//  measurementId: "G-G0J7EQCZPC"
+//};
+
+ const firebaseConfig = {
+  apiKey: "AIzaSyDUlZybnaoLh4jjIKN-cGG7y1QRGKs1Onw",
+  authDomain: "iml300-firebase-demo-24901.firebaseapp.com",
+  projectId: "iml300-firebase-demo-24901",
+  storageBucket: "iml300-firebase-demo-24901.firebasestorage.app",
+  messagingSenderId: "118840852627",
+  appId: "1:118840852627:web:bc7fac75040955f2f1cd66",
+  measurementId: "G-WRMVPBTJCD"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
+let dbRef = db.ref("text");
+
+//  var data ={
+//    name: "q",
+//    word: "hello"
+//  }
+
+// dbRef.push(data);
+
+let chatContainer = document.getElementById("chat-container");
+let entry = document.getElementById("text-input-entry");
+let share = document.getElementById("text-input-submit");
+
+dbRef.on("child_added", gotText);
+
+function gotText(data) {
+  let id = data.key;
+  let value = data.val();
+  console.log(value);
+  chatContainer.innerHTML =
+    "<div class='response'>" + value + "</div>" + chatContainer.innerHTML;
+}
+
+//click button will run this function
+const textInputSubmit = document.getElementById("text-input-submit");
+textInputSubmit.addEventListener("click", submitText);
+
+let textContainerElement = document.getElementById("text-input-entry");
+
+function submitText() {
+  let textToSubmit = textContainerElement.value; //gets text value from textbox
+  let newKey = dbRef.push().key; //ask firebase to give you a new key / 'name'
+  let updates = {}; //send firebase list of values
+  updates[newKey] = textToSubmit;
+  dbRef.update(updates);
+}
+
+function submitlock() {
+  entry.remove();
+  share.value = "Thanks for telling me.";
+  share.disabled = true;
+  share.style.width = "70%";
+}
