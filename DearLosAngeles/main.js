af46f10e0b92bacc990ea601eedf386cebcaa772
leeechloe <celee@usc.edu>
2026-04-27 23:53:28 -0700

    new edits

---
+++ DearLosAngeles/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $(function () {
+        $("communitygoods").draggable();
+    });   
+});
+
+$(document).ready(function () {
+    $(function () {
+        $("").draggable();
+    });   
+});
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDY_YPuqgZuOCtzT3KpEsgRL_NHzhCMXwE",
+  authDomain: "dearlosan.firebaseapp.com",
+  projectId: "dearlosan",
+  storageBucket: "dearlosan.firebasestorage.app",
+  messagingSenderId: "456497416113",
+  appId: "1:456497416113:web:d1270bad53c7294b67df90",
+  measurementId: "G-K107WQJL70"
+};
+
